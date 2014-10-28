2e604343d2348e097a17085458dd72fd0d5248a5
thinktt <github@thinktt.e4ward.com>
2014-10-28 14:20:22 -0500

    initial commit

---
+++ buildings.js
@@ -0,0 +1,51 @@
+var buildings = [],
+		water = 0,
+    i, j, left, right,
+    pool, getRandomInt,
+    pools = []; 
+
+getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+
+for(i=0; i<10; i++) {
+	buildings.push(getRandomInt(1,10));
+}
+
+
+
+console.log(buildings);
+
+while(buildings.length > 0) {
+
+	while (buildings[0]<=buildings[1]) {
+		buildings.shift();
+	}
+
+	for(i=1; buildings[0] > buildings[i]; i++);
+	i++;
+	pool = buildings.slice(0, i); 
+	if(pool.length > 2) pools.push(pool);
+	buildings = buildings.slice(i);
+
+}
+
+console.log();
+console.log(pools);
+
+
+
+
+
+//divide array into pools
+
+//find the first tallest
+//what's the tallest building that's not taller or equal
+
+//first number is left
+
+//if number next to left is higher next becomes left
+
+
+
