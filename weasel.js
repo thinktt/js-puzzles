6932cd7c106790dc9f0711013313adc71f24a21f
thinktt <github@thinktt.e4ward.com>
2014-12-12 16:52:59 -0600

    finished Dawkin's Weasel

--- weasel.js
+++ weasel.js
@@ -7,5 +7,84 @@ If any of the new strings has a perfect score (28), halt. Otherwise, take the hi
 */
 
 
-var baseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ',
-		i=0;
+//.....Declirations and Intializers..........
+var BASE_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ',
+		target = 'METHINKS IT IS LIKE A WEASEL',
+		i, j,
+		fittest = [],
+		offspring = [],
+		top = {},
+		itterations = 0;
+
+
+//...........Functions....................
+var getRandInt, reproduce;
+
+getRandInt = function(min, max) {
+  return Math.floor(Math.random() * ((max + 1) - min)) + min;
+};
+
+reproduce = function(fittest, target) {
+	var	arr = [],
+			score = 0,
+			chance, i;
+
+	for(i=0; i<target.length; i++) {
+			chance = getRandInt(1,20);
+			if(chance === 1) {
+				arr[i] = BASE_CHARS.charAt(getRandInt(0,26));
+				//console.log('Mutants at ' + i + '!');
+			}
+			else arr[i] = fittest[i];
+			if(arr[i] === target[i]) score++;
+	}
+
+	return {
+		arr: arr,
+		score: score
+	};
+};
+
+
+//............Main.........................
+//build 2D array for offspring var
+for(i=0; i<100; i++) {
+	offspring[i] = [];
+}
+
+//build a random array of chars
+for(i=0; i<target.length; i++) {
+	fittest[i] = BASE_CHARS.charAt(getRandInt(0,26));
+}
+
+
+do {
+	itterations++;
+	//creat 100 offspring, along with a score for each
+	top.score = 0;
+	top.index = 0; 
+	
+	for (i=0; i<100; i++) {
+		offspring[i] = reproduce(fittest, target); 
+		if(offspring[i].score > top.score) {
+			top.score = offspring[i].score;
+			top.index = i; 
+		}
+	}
+
+	//crown new fittest 
+	fittest = offspring[top.index].arr;
+} while (top.score < 28);
+
+
+
+for (i=0; i<100; i++) {
+	console.log(offspring[i].arr.join('') + ' ' + offspring[i].score);
+}
+
+console.log('Top Score: ' + top.score); 
+console.log('Top Index: ' + top.index); 
+console.log('New Fittest: ' + fittest.join(''));
+console.log('Itterations: ' + itterations);
+console.log();
+
